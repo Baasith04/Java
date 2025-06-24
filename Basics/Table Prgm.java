64abc8afa3b8c257a80d0f3ad5dc99a139db4e32
Baasith04 <baasithsulaiman2004@gmail.com>
2025-06-24 14:29:59 +0530

    Create Table Prgm.java

---
+++ Basics/Table Prgm.java
@@ -0,0 +1,16 @@
+package java_YM;
+import java.util.Scanner;
+
+public class Prgm3 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter num: ");
+		int num = sc.nextInt();
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(num+" x "+i+" = "+(i*num));
+		}
+		sc.close();
+	}
+
+}
