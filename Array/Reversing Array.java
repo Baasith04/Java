61143b2090dcb5c18edafe059ec9380378033eee
Baasith04 <baasithsulaiman2004@gmail.com>
2025-06-25 12:14:00 +0530

    Create Reversing Array.java

---
+++ Array/Reversing Array.java
@@ -0,0 +1,22 @@
+package java_YM;
+import java.util.Scanner;
+
+public class Array1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Size of Array: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.print("Enter "+n+" Elements: ");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		for(int i=n-1;i>=0;i--)
+		{
+			System.out.print(arr[i]+" ");
+		}
+		sc.close();
+		
+	}
+}
