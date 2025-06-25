bdbdecabf41bfa4a9323a066ef133799e2e1ee0c
Baasith04 <baasithsulaiman2004@gmail.com>
2025-06-25 11:08:38 +0530

    Create Average.java

---
+++ Array/Average.java
@@ -0,0 +1,26 @@
+package java_YM;
+import java.util.Scanner;
+
+public class Array1 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Size of Array: ");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		int sum=0;
+		System.out.print("Enter "+n+" Elements: ");
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		for(int i=0;i<n;i++)
+		{
+			sum+=arr[i];
+		}
+		double avg = (double)sum/n;
+		System.out.println("Average of Array Elements are: "+avg);
+		sc.close();
+		
+	}
+}
