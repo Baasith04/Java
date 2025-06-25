263972e66423130d23549fb60f084d18493ad589
Baasith04 <baasithsulaiman2004@gmail.com>
2025-06-25 12:00:31 +0530

    Update and rename Sorting to Sorting.java

--- Array/Sorting.java
+++ Array/Sorting.java
@@ -16,7 +16,7 @@ public class Array1 {
 		{
 			for(int j=i+1;j<n-1;j++)
 			{
-				if(arr[i]>arr[j])
+				if(arr[i]>arr[j]) // for descending arr[i]<arr[j]
 				{
 					int temp = arr[i];
 					arr[i] = arr[j];
